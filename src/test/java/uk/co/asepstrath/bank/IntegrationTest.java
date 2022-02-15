48fec05e97d636c37a91904fab5a730cc1ed7278
Kamil Zak <kamilzak00@gmail.com>
2022-02-15 17:49:41 +0000

    fixed integration test

--- src/test/java/uk/co/asepstrath/bank/IntegrationTest.java
+++ src/test/java/uk/co/asepstrath/bank/IntegrationTest.java
@@ -29,13 +29,14 @@ public class IntegrationTest {
         }
     }
 
+    @Test
     public void showAccount(int serverPort) throws IOException {
         Request req = new Request.Builder()
                 .url("http://localhost:" + serverPort + "/accounts/get")
                 .build();
 
         try (Response rsp = client.newCall(req).execute()) {
-            assertEquals("{accounts=accounts, user=[{name=Rachel, balance=50.0}, {name=Monica, balance=100.0}, {name=Phoebe, balance=76.0}, {name=Joey, balance=23.9}, {name=Chandler, balance=3.0}, {name=Ross, balance=54.32}]}", rsp.body().string());
+            assertEquals("<!DOCTYPE html><head> <meta charset=\"utf-8\"> <title></title> <meta name=\"description\" content=\"\"> <meta name=\"viewport\" content=\"width=device-width, initial-scale=1\"> <title>GET Endpoint — Test</title></head><body> <table> <tr> <th>First Name</th> <th>Balance</th> </tr><tr> <td>Rachel</td><td>50.0</td></tr><tr> <td>Monica</td><td>100.0</td></tr><tr> <td>Phoebe</td><td>76.0</td></tr><tr> <td>Joey</td><td>23.9</td></tr><tr> <td>Chandler</td><td>3.0</td></tr><tr> <td>Ross</td><td>54.32</td></tr></table></body></html>", rsp.body().string());
             assertEquals(StatusCode.OK.value(), rsp.code());
         }
     }
