6b0061fb19b6c136368156bd4e25b7596ee4216b
Kamil Zak <kamilzak00@gmail.com>
2022-02-24 07:35:53 +0000

    feat: account data is fetched from swagger

--- src/main/java/uk/co/asepstrath/bank/Controller.java
+++ src/main/java/uk/co/asepstrath/bank/Controller.java
@@ -6,20 +6,26 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import io.jooby.ModelAndView;
 import io.jooby.annotations.GET;
 import io.jooby.annotations.Path;
+import kong.unirest.json.JSONArray;
+import kong.unirest.json.JSONObject;
 import org.slf4j.Logger;
 
 import javax.sql.DataSource;
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-
 //Path = IP/argument, i.e("localhost:8080/accounts)
 @Path("/accounts")
 public class Controller {
 
+    private static HttpURLConnection urlConn;
     private final DataSource dataSource;
     private final Logger logger;
 
@@ -68,5 +74,49 @@ public class Controller {
         mapTest.put("user", listData);
 
         return new ModelAndView("accounts.hbs", mapTest);
+    }
+
+    @GET("/test")
+    public int fetchData() throws IOException {
+        BufferedReader connectionReader;
+        StringBuffer responseContent = new StringBuffer();
+        String line;
+        int status;
+
+        try {
+            URL url = new URL("https://api.asep-strath.co.uk/api/Team1/accounts");
+            urlConn = (HttpURLConnection) url.openConnection();
+            urlConn.setRequestMethod("GET");
+
+            status = urlConn.getResponseCode();
+
+            if (status == 200) {
+                connectionReader = new BufferedReader(new InputStreamReader(urlConn.getInputStream()));
+                while((line = connectionReader.readLine()) != null) {
+                    responseContent.append(line);
+                }
+                connectionReader.close();
+            } else {
+                connectionReader = new BufferedReader(new InputStreamReader(urlConn.getErrorStream()));
+                while((line = connectionReader.readLine()) != null) {
+                    responseContent.append(line);
+                }
+                connectionReader.close();
+            }
+            parseJson(responseContent.toString());
+        }  catch (IOException e) {} finally {
+            urlConn.disconnect();
+        }
+
+        return 0;
+    }
+
+    public static String parseJson(String responseBody) {
+        JSONArray accountsData = new JSONArray(responseBody);
+        for (int i = 0; i < accountsData.length(); i++) {
+            JSONObject accountData = accountsData.getJSONObject(i);
+            System.out.println(accountData);
+        }
+        return "a";
     }
 }
