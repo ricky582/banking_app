0bc82b45b5584225c55f31219b3b62561077f4d1
robert <robert.sim.2020@uni.strath.ac.uk>
2022-03-09 02:22:09 +0000

    Fixed code smells in Account and AccountTests

--- src/test/java/uk/co/asepstrath/bank/AccountsTest.java
+++ src/test/java/uk/co/asepstrath/bank/AccountsTest.java
@@ -3,51 +3,50 @@ package uk.co.asepstrath.bank;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-public class AccountsTest {
+class AccountsTest {
     @Test
-    public void testGetBalance()
+    void testGetBalance()
     {
         Account a = new Account("Fred", 45.3);
         Account b = new Account("1232", "Garry", 56.3, "Investment Account", "GBP");
         Account c = new Account("1234");
-        Assertions.assertEquals(a.getBalance(), 45.3);
-        Assertions.assertEquals(b.getBalance(), 56.3);
-        Assertions.assertEquals(c.getBalance(), -1);
+        Assertions.assertEquals(45.3, a.getBalance());
+        Assertions.assertEquals(56.3, b.getBalance());
+        Assertions.assertEquals(-1, c.getBalance());
     }
 
     @Test
-    public void testGetName(){
+    void testGetName(){
         Account a = new Account("Fred", 45.3);
         Account b = new Account("1232", "Garry", 56.3, "Investment Account", "GBP");
         Account c = new Account("1234");
-        Assertions.assertEquals(a.getName(), "Fred");
-        Assertions.assertEquals(b.getName(), "Garry");
-        Assertions.assertEquals(c.getName(), "");
+        Assertions.assertEquals("Fred", a.getName());
+        Assertions.assertEquals("Garry", b.getName());
+        Assertions.assertEquals("", c.getName());
     }
 
     @Test
-    public void testGetAccountType(){
+    void testGetAccountType(){
         Account a = new Account("Fred", 45.3);
         Account b = new Account("1232", "Garry", 56.3, "Investment Account", "GBP");
         Account c = new Account("1234");
         Assertions.assertNull(a.getAccountType());
-        Assertions.assertEquals(b.getAccountType(), "Investment Account");
-        Assertions.assertEquals(c.getAccountType(), "External");
+        Assertions.assertEquals("Investment Account", b.getAccountType());
+        Assertions.assertEquals("External", c.getAccountType());
     }
 
     @Test
-    public void testGetCurrency(){
+    void testGetCurrency(){
         Account a = new Account("Fred", 45.3);
         Account b = new Account("1232", "Garry", 56.3, "Investment Account", "GBP");
         Account c = new Account("1234");
         Assertions.assertNull(a.getCurrency());
-        Assertions.assertEquals(b.getCurrency(), "GBP");
-        Assertions.assertEquals(c.getCurrency(), "");
+        Assertions.assertEquals("GBP", b.getCurrency());
+        Assertions.assertEquals("", c.getCurrency());
     }
 
     @Test
-    public void testNotNull()
-    {
+    void testNotNull() {
         Account a = new Account("Fred", 45.3);
         Account b = new Account("1232", "Garry", 56.3, "Investment Account", "GBP");
         Account c = new Account("1234");
@@ -58,31 +57,27 @@ public class AccountsTest {
     }
 
     @Test
-    public void testDeposit()
-    {
+    void testDeposit() {
         Account a = new Account("a",20);
         a.deposit(50);
         Assertions.assertEquals(70, a.getBalance());
     }
 
     @Test
-    public void testWithdraw()
-    {
+    void testWithdraw() {
         Account a = new Account("a",40);
         a.withdraw(20);
         Assertions.assertEquals(20,a.getBalance());
     }
 
     @Test
-    public void testNoOverdraft()
-    {
+    void testNoOverdraft() {
         Account a = new Account("a",30);
         Assertions.assertThrows(ArithmeticException.class,() -> a.withdraw(100));
     }
 
     @Test
-    public void testMultipleTransactions()
-    {
+    void testMultipleTransactions() {
         Account a = new Account("a",20);
         for(int i = 0; i < 5; i++) {
             a.deposit(10);
@@ -96,20 +91,19 @@ public class AccountsTest {
     }
 
     @Test
-    public void testDecimal()
-    {
+    void testDecimal() {
         Account a = new Account("a",5.45);
         a.deposit(17.56);
         Assertions.assertEquals(23.01, a.getBalance());
     }
 
     @Test
-    public void testNewToString(){
+    void testNewToString(){
         Account a = new Account("1234");
         Account b = new Account("5678", "test", 100.76, "Test", "GBP");
         Account NULL = null;
-        Assertions.assertEquals(a.toString(), "1234 false");
-        Assertions.assertEquals(b.toString(), "5678 test 100.76 Test GBP");
+        Assertions.assertEquals(a.toString(), "1234 false", a.toString());
+        Assertions.assertEquals(b.toString(), "5678 test 100.76 Test GBP",b.toString());
         Assertions.assertThrows(NullPointerException.class, () -> NULL.toString());
     }
 }
